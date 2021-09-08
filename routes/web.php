2c40006c155a427ba7dc98bd51582b1069cf9057
FitriPurnama <fitri.purnama@peruridigit.co.id>
2021-09-08 13:53:21 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/tcpdf', [App\Http\Controllers\HomeController::class, 'createPDF'])->name('createPDF');
